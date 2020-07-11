14e4e25ae31d4dfecfaf4ba5aeaa00896994f275
daevlin <55744916+daevlin@users.noreply.github.com>
2020-07-11 15:02:36 +0200

    Update download_cruloader_payload.py

--- download_cruloader_payload.py
+++ download_cruloader_payload.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-#Todo brute XOR instead of static hex value
+#Todo brute XOR instead of static hex value, possibly using malduck Yara
 #Todo 2 trim de-XOR:ed MZ file. For now I am piping the download payload througth the tool "cut-bytes.py '[4D5A90]':" from Didier Stevens.
 
 import requests
