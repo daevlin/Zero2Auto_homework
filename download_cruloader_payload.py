a28216c0eda9433d841c0d40bb795e1e358c4c75
daevlin <55744916+daevlin@users.noreply.github.com>
2020-07-11 14:55:36 +0200

    Update download_cruloader_payload.py

--- download_cruloader_payload.py
+++ download_cruloader_payload.py
@@ -11,7 +11,7 @@ url = 'https://pastebin.com/raw/mLem9DGk'
 
 #Change the User-Agent to look less suspicius
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36'
+    'User-Agent': 'cruloader'
 }
 
 r = requests.get(f'{url}', headers=headers)
