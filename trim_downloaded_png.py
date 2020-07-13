0dfcef9c46bbff068655d3e8ea423563f09051f5
daevlin <55744916+daevlin@users.noreply.github.com>
2020-07-13 12:31:43 +0200

    Update trim_downloaded_png.py

--- trim_downloaded_png.py
+++ trim_downloaded_png.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python
-
-#Only works with Python2 atm. Will have to make it work with Python3 before adding the code to cruloader_unpacker_downloader.py
+#!/usr/bin/env python3
 
 import sys
 import malduck
@@ -9,14 +7,14 @@ infile = sys.argv[1]
 
 with open(infile,'rb') as f:
 	payload = f.read()
+#.decode('latin1')
 
-png_marker = 'redaolurc'	
+png_marker = bytes('redaolurc', 'latin1')	
 m = payload.find(png_marker)
 png_marker_len = len('redaolurc')
 
 trimmed_file = (payload[m+png_marker_len:])
 dexor = malduck.xor(0x61, trimmed_file)
 
-with open('cruloader_final_payload.bin', 'wb') as o:
+with open('trimmed.bin', 'wb') as o:
 	o.write(dexor)
-	
